e845cb35f3b9399a4ee731c0a19c455c06baa857
Samruddhi <guravsamruddhi2006@gmail.com>
2026-08-12 23:24:28 +0530

    Time: 69 ms (43.18%), Space: 88.9 MB (56.07%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,43 +1,18 @@
-// class Solution {
-//     public int maxSubarrayLength(int[] nums, int k) {
-//         int n = nums.length;
-//         int l = 0;
-//         int maxlength = 0;
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for(int i = 0; i<n; i++){
-//             map.put(nums[i], map.getOrDefault(nums, 0)+1);
-//             while(map.get(nums[i])>k){
-//                 map.put(nums[l], map.get(nums[l])-1);
-//                 l++;
-//             }
-
-//             maxlength = Math.max(maxlength, i-l+1);
-//         }
-//         return maxlength;
-//     }
-// }
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0;
+        int maxlength = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        int left = 0;
-        int maxLen = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-
-            // Add nums[right]
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-
-            // Shrink window if frequency exceeds k
-            while (map.get(nums[right]) > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                left++;
+        for(int i = 0; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            while(map.get(nums[i])>k){
+                map.put(nums[l], map.get(nums[l])-1);
+                l++;
             }
 
-            // Current window is valid
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxlength = Math.max(maxlength, i-l+1);
         }
-
-        return maxLen;
+        return maxlength;
     }
 }
