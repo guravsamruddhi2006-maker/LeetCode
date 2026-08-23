2567426cca7092de8b6bc7a18b33582068c35f8e
Samruddhi <guravsamruddhi2006@gmail.com>
2026-08-23 21:28:43 +0530

    Time: 8 ms (53.28%), Space: 47.2 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        int qDiff = leftQ - rightQ;
+        int sumDiff = leftSum - rightSum;
+
+        // Odd number of unmatched '?'
+        if (Math.abs(qDiff) % 2 == 1) {
+            return true;
+        }
+
+        // Important: DON'T use abs(sumDiff) here
+        return sumDiff != -9 * qDiff / 2;
+    }
+}
